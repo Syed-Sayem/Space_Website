6e574c79232fcd93058d592ab03d6c09f4770a25
Syed Ekramul Khayer Sayem <168598535+Syed-Sayem@users.noreply.github.com>
2024-11-27 08:47:24 +0530

    Add files via upload

---
+++ Banner.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import sateliteImg from "../../assets/satelite1.jpg";
+
+const Banner = () => {
+  return (
+    <div className='bg-black text-white pb-1 relative z-50'>
+      <div className='container'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 items-center'>
+          <div>
+            <img data-aos="zoom-in" src={sateliteImg} alt="" 
+            className='w-full sm:w-[80%] mx-auto max-h-[350px]object-cover'/>
+          </div>
+          <div className='space-y-3 xl:pr-36 p-4 border-r-2 border-b-2 border-r-sky-800 border-b-sky-800'>
+            <p data-aos="fade-up"  className='text-sky-800 uppercase'>Our Mission</p>
+            <h1 data-aos='fade-up' data-aos-delay="300" className='uppercase text-5xl'>Rapidcast</h1>
+            <p data-aos='fade-up' data-aos-delay="500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium commodi architecto dolore qui animi placeat ipsam reiciendis sint nulla! Maiores a modi suscipit velit illo quo tempora omnis quidem commodi?
+            </p>
+            <button data-aos='fade-up' data-aos-delay="700" className='primary-button'>Learn More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner;
